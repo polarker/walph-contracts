ec587014b45c0d2cc1e8e79f61f013e127a2126c
cgi-bin <s@ono.re>
2023-09-14 21:01:02 +0200

    add: keep fees in the contract

---
+++ test/integration/walph.test.ts
@@ -0,0 +1,263 @@
+import { web3, Project, stringToHex, ONE_ALPH, DUST_AMOUNT, sleep, ZERO_ADDRESS, sign } from '@alephium/web3'
+import { NodeWallet, PrivateKeyWallet } from '@alephium/web3-wallet'
+import { Walph, Buy, Open,Close, WalphTypes, Destroy, BuyWithoutToken, WithdrawFees } from '../../artifacts/ts'
+import configuration, { Settings } from '../../alephium.config'
+import * as dotenv from 'dotenv'
+import { waitTxConfirmed } from '@alephium/cli'
+import { getSigner, testPrivateKey } from '@alephium/web3-test'
+
+dotenv.config()
+
+
+let signer
+let signerAddress
+let rndSignerBuy
+let testGroup
+
+const tokenIdToHold = "3f52b6bdb8678b8931d683bbae1bd7c5296f70a2ab87bbd1792cb24f9b1d1500"
+const networkToUse = 'devnet'
+
+describe('integration tests', () => {
+  beforeAll(async () => {
+    web3.setCurrentNodeProvider(configuration.networks[networkToUse].nodeUrl, undefined, fetch)
+    await Project.build()
+  
+ // rndSignerBuy = new PrivateKeyWallet({privateKey: configuration.networks[networkToUse].privateKeys[1] , keyType: undefined, nodeProvider: web3.getCurrentNodeProvider()})
+
+  signer = new PrivateKeyWallet({ privateKey: testPrivateKey })
+
+  signerAddress = await signer.account.address
+  testGroup = signer.account.group
+  
+  })
+  
+
+  it('should test Walph', async () => {
+    
+    const deployResult = await Walph.deploy(
+      signer,
+      {
+        initialFields: {
+            poolSize: 10n * 10n ** 18n,
+            poolOwner: signerAddress,
+            poolFees: 1n,
+            ticketPrice: 10n ** 18n,
+            minTokenAmountToHold: 0n,
+            tokenIdToHold: tokenIdToHold,
+            open: true,
+            balance: 0n,
+            feesBalance: 0n,
+            numAttendees: 0n,
+            attendees: Array(10).fill(ZERO_ADDRESS) as WalphTypes.Fields["attendees"],
+            lastWinner: ZERO_ADDRESS
+
+          },
+      }
+    )
+
+    
+    const walph = deployResult.contractInstance
+    const walphleContractId = deployResult.contractInstance.contractId
+    const walphContractAddress = deployResult.contractInstance.address
+
+    console.log("Contract deployed at: "+walphleContractId+" "+walphContractAddress)
+    expect(deployResult.contractInstance.groupIndex).toEqual(testGroup)
+
+    const walphleDeployed = Walph.at(walphContractAddress)
+
+
+    const initialState = await walphleDeployed.fetchState()
+    const initialBalance = initialState.fields.balance
+    expect(initialBalance).toEqual(0n)
+
+    const getPoolSize = await walph.methods.getPoolSize()
+    expect(getPoolSize.returns).toEqual(10n * 10n ** 18n)
+  
+    const ticketBoughtEvents: WalphTypes.TicketBoughtEvent[] = []
+    const subscription = walph.subscribeTicketBoughtEvent({
+      pollingInterval: 1,
+      messageCallback: async (event: WalphTypes.TicketBoughtEvent) => {
+        ticketBoughtEvents.push(event)
+        return Promise.resolve()
+      },
+      errorCallback: async (error, subscription) => {
+        console.error(error)
+        subscription.unsubscribe()
+        return Promise.resolve()
+      }
+    })
+
+      const contractBalance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(walphContractAddress)
+      expect(parseInt(contractBalance.balance)).toBeGreaterThanOrEqual(ONE_ALPH)
+
+      const lastOne = await getSigner()
+      // simulate someone buying tickets
+      for (let i = 0; i < 9; i++) {
+        await BuyWithoutToken.execute(signer, {
+          initialFields: {walphContract: walphleContractId , amount: ONE_ALPH},
+          attoAlphAmount:  ONE_ALPH + 2n * DUST_AMOUNT,
+          
+        })
+        
+      }
+
+      const afterPoolFull = await walphleDeployed.fetchState()
+      const afterPoolFullOpenState = afterPoolFull.fields.open
+      const afterPoolFullBalanceState = afterPoolFull.fields.balance
+      const afterPoolFullNumAttendeesState = afterPoolFull.fields.numAttendees
+      const afterPoolFullAttendeesState = afterPoolFull.fields.attendees
+
+
+      console.log("Pool state: "+afterPoolFullOpenState + " Balance: "+afterPoolFullBalanceState/10n**18n+ " Attendees: " + afterPoolFullAttendeesState)
+      let expectedArray = Array(9).fill(signer.address) as WalphTypes.Fields["attendees"]
+      expectedArray[9] = ZERO_ADDRESS
+
+      expect(afterPoolFullOpenState).toEqual(true)
+      expect(afterPoolFullBalanceState).toEqual(9n * 10n ** 18n)
+      expect(afterPoolFullNumAttendeesState).toEqual(9n)
+      expect(afterPoolFullAttendeesState).toEqual(expectedArray)
+
+
+      expect(ticketBoughtEvents.length).toEqual(9)
+      
+      //buy last ticket to draw the pool
+      await BuyWithoutToken.execute(signer, {
+        initialFields: {walphContract: walphleContractId , amount: ONE_ALPH},
+        attoAlphAmount: ONE_ALPH + DUST_AMOUNT,
+        
+      })
+
+      const contractAfterPoolDistributionBalance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(walphContractAddress)
+      expect(contractAfterPoolDistributionBalance.balanceHint).toEqual("1.09 ALPH")
+      const winnerBalance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(signer.address)
+
+
+      const afterPoolDistribution = await walphleDeployed.fetchState()
+      const afterPoolDistributionOpenState = afterPoolDistribution.fields.open
+      const afterPoolDistributionBalanceState = afterPoolDistribution.fields.balance
+      const afterPoolDistributionNumAttendeesState = afterPoolDistribution.fields.numAttendees
+      const afterPoolDistributionWinner = afterPoolDistribution.fields.lastWinner
+
+      console.log("Pool state: "+afterPoolDistributionOpenState + " Balance: "+afterPoolDistributionBalanceState/10n**18n + " Fields: "+ JSON.stringify(afterPoolDistribution.fields))
+      expect(afterPoolDistributionOpenState).toEqual(true)
+      expect(afterPoolDistributionBalanceState).toEqual(0n)
+      expect(afterPoolDistributionNumAttendeesState).toEqual(0n)
+      expect(afterPoolDistributionWinner).toEqual(signer.account.address)
+
+      subscription.unsubscribe()
+
+      await WithdrawFees.execute(signer, {
+        initialFields: { walphContract: walphleContractId},
+        attoAlphAmount: DUST_AMOUNT
+      })
+
+      const afterWhitdraw = await walphleDeployed.fetchState()
+      expect(afterWhitdraw.fields.feesBalance).toEqual(0n)
+      const afterWhitdrawBalance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(walphContractAddress)
+      expect(afterWhitdrawBalance.balanceHint).toEqual("1 ALPH")
+
+     await Destroy.execute(signer, {
+        initialFields: { walphContract: walphleContractId},
+        attoAlphAmount: DUST_AMOUNT
+
+      })
+      /*
+      const contractBalance = await web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(walphContractAddress)
+      expect(web3.getCurrentNodeProvider().addresses.getAddressesAddressBalance(walphContractAddress)).toEqual(0n)*/
+
+  }
+)
+
+
+it('should close and open pool', async () => {
+
+
+  const deployResult = await Walph.deploy(
+    signer,
+    {
+      initialFields: {
+          poolSize: 10n * 10n ** 18n,
+          poolOwner: signerAddress,
+          poolFees: 1n,
+          minTokenAmountToHold: 0n,
+          ticketPrice: 10n ** 18n,
+          tokenIdToHold: tokenIdToHold,
+          open: true,
+          balance: 0n,
+          feesBalance: 0n,
+          numAttendees: 0n,
+          attendees: Array(10).fill(ZERO_ADDRESS) as WalphTypes.Fields["attendees"],
+          lastWinner: ZERO_ADDRESS
+
+        },
+    }
+  )
+
+  
+  const walph = deployResult.contractInstance
+  const walphleContractId = deployResult.contractInstance.contractId
+  const walphContractAddress = deployResult.contractInstance.address
+
+  console.log("Contract deployed at: "+walphleContractId+" "+walphContractAddress)
+  expect(deployResult.contractInstance.groupIndex).toEqual(testGroup)
+
+  const walphleDeployed = Walph.at(walphContractAddress)
+
+  const closePoolEvents: WalphTypes.PoolCloseEvent[] = []
+      const subscription = walph.subscribePoolCloseEvent({
+        pollingInterval: 10,
+        messageCallback: async (event: WalphTypes.PoolCloseEvent) => {
+          closePoolEvents.push(event)
+          return Promise.resolve()
+        },
+        errorCallback: async (error, subscription) => {
+          console.error(error)
+          subscription.unsubscribe()
+          return Promise.resolve()
+        }
+      })
+
+  await Close.execute(signer, {
+    initialFields: {walphContract: walphleContractId}
+  })
+
+
+  const closePoolState = await walphleDeployed.fetchState()
+  const closeState = closePoolState.fields.open
+  expect(closeState).toEqual(false)
+
+  expect(closePoolEvents.length).toEqual(1)
+  subscription.unsubscribe()
+
+  const openPoolEvents: WalphTypes.PoolOpenEvent[] = []
+  const subscriptionPoolOpen = walph.subscribePoolCloseEvent({
+    pollingInterval: 10,
+    messageCallback: async (event: WalphTypes.PoolOpenEvent) => {
+
+      openPoolEvents.push(event)
+      return Promise.resolve()
+    },
+    errorCallback: async (error, subscriptionPoolOpen) => {
+      console.error(error)
+      subscriptionPoolOpen.unsubscribe()
+      return Promise.resolve()
+    }
+  })
+
+
+  await Open.execute(signer, {
+    initialFields: {walphContract: walphleContractId}
+  })
+  
+  const poolState = await walphleDeployed.fetchState()
+  const openState = poolState.fields.open
+  expect(openState).toEqual(true)
+  
+  expect(openPoolEvents.length).toEqual(1)
+  subscriptionPoolOpen.unsubscribe()
+
+
+})
+
+
+})
---
+++ test/unit/walph.test.ts
@@ -0,0 +1,355 @@
+import {
+  web3,
+  Project,
+  TestContractParams,
+  addressFromContractId,
+  AssetOutput,
+  DUST_AMOUNT,
+  addressFromPublicKey,
+  ZERO_ADDRESS,
+  ContractState,
+  NamedVals,
+  sleep,
+} from "@alephium/web3";
+import { PrivateKeyWallet } from "@alephium/web3-wallet";
+import {
+  expectAssertionError,
+  randomContractId,
+  testAddress,
+  testPrivateKey,
+} from "@alephium/web3-test";
+import { Walph, WalphTypes } from "../artifacts/ts";
+
+describe("unit tests", () => {
+  let testContractId: string;
+  let testTokenId: string;
+  let testContractAddress: string;
+  let testParamsFixture: TestContractParams<
+    WalphTypes.Fields,
+    { amount: bigint }
+  >;
+
+  // We initialize the fixture variables before all tests
+  beforeAll(async () => {
+    web3.setCurrentNodeProvider("http://127.0.0.1:22973", undefined, fetch);
+    await Project.build();
+    testContractId = randomContractId();
+
+    testTokenId = testContractId;
+    testContractAddress = addressFromContractId(testContractId);
+    testParamsFixture = {
+      // a random address that the test contract resides in the tests
+      address: testContractAddress,
+
+      // initial state of the test contract
+      initialFields: {
+        poolSize: 10n * 10n ** 18n,
+        poolOwner: testAddress,
+        poolFees: 1n,
+        minTokenAmountToHold: 1n,
+        ticketPrice: 10n ** 18n,
+        tokenIdToHold:
+          "47504df5a7b18dcecdbf1ea00b7e644d0a7c93919f2d2061ba153f241f03b801",
+        open: false,
+        balance: 0n,
+        feesBalance: 0n,
+        numAttendees: 0n,
+        attendees: Array(10).fill(
+          ZERO_ADDRESS
+        ) as WalphTypes.Fields["attendees"],
+        lastWinner: ZERO_ADDRESS,
+      },
+      initialAsset: {
+        alphAmount: 1n * 10n ** 18n,
+      },
+      // arguments to test the target function of the test contract
+      testArgs: {
+        amount: 1n * 10n ** 18n,
+      },
+      // assets owned by the caller of the function
+      inputAssets: [
+        {
+          address: testAddress,
+          asset: {
+            alphAmount: 100n * 10n ** 18n,
+            tokens: [
+              {
+                id: "47504df5a7b18dcecdbf1ea00b7e644d0a7c93919f2d2061ba153f241f03b801",
+                amount: 2n,
+              },
+            ],
+          },
+        },
+      ],
+    };
+  });
+
+  it("test try random", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.numAttendees = 10
+    let runs = [0n,0n]
+
+    // open the pool
+    let testResult = await Walph.tests.random(testParams);
+    runs[0] = testResult.returns
+    sleep(1000)
+    testResult = await Walph.tests.random(testParams);
+
+    runs[1] = testResult.returns
+    console.log(runs)
+
+    expect(runs[0]).not.toEqual(runs[1])
+
+  })
+
+  it("test opening and closing pool", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+
+    // open the pool
+    let testResult = await Walph.tests.openPool(testParams);
+    let contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.address).toEqual(testContractAddress);
+    expect(contractState.fields.open).toEqual(true);
+
+    //assign new state to initial fields and close the pool
+    testParams.initialFields = contractState.fields;
+    testResult = await Walph.tests.closePool(testParams);
+
+    contractState = testResult.contracts[0] as WalphTypes.State;
+    expect(contractState.fields.open).toEqual(false);
+  });
+
+  it("test try to open an opened pool", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+
+    await expectAssertionError(
+      Walph.tests.openPool(testParams),
+      testContractAddress,
+      2
+    );
+  });
+
+  it("test try to close a closed pool", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = false;
+
+    await expectAssertionError(
+      Walph.tests.closePool(testParams),
+      testContractAddress,
+      1
+    );
+  });
+
+  it("test try to open the pool with wrong address", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    const wrongAddress = PrivateKeyWallet.Random(
+      0,
+      web3.getCurrentNodeProvider()
+    ).account.address;
+    testParams.inputAssets[0].address = wrongAddress;
+    testParams.initialFields.open = false;
+
+    await expectAssertionError(
+      Walph.tests.openPool(testParams),
+      testContractAddress,
+      4
+    );
+  });
+
+  it("test buy a ticket", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+
+    const testResult = await Walph.tests.buyTicket(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.balance).toEqual(10n ** 18n);
+    expect(contractState.fields.feesBalance).toEqual(10n ** 16n)
+    expect(contractState.fields.attendees.length).toEqual(10);
+    expect(contractState.fields.numAttendees).toEqual(1n);
+    expect(contractState.fields.attendees).toEqual([
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS, 
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+    ]);
+  });
+
+
+  it("test buy 5 tickets", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+    testParams.testArgs.amount = 5n * 10n ** 18n
+    const testResult = await Walph.tests.buyTicket(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.balance).toEqual(5n * 10n ** 18n);
+    expect(contractState.fields.feesBalance).toEqual(5n * 10n ** 16n)
+    expect(contractState.fields.attendees.length).toEqual(10);
+    expect(contractState.fields.numAttendees).toEqual(5n);
+    expect(contractState.fields.attendees).toEqual([
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+    ]);
+  });
+
+
+  it("test buy 3 tickets after 5 was bought", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.initialFields.balance = 5n * 10n ** 18n;
+    testParams.initialFields.numAttendees = 5n;
+    testParams.initialFields.feesBalance = 5n * 10n ** 16n
+
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+    testParams.testArgs.amount = 3n * 10n ** 18n
+    const testResult = await Walph.tests.buyTicket(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.balance).toEqual(8n * 10n ** 18n);
+    expect(contractState.fields.feesBalance).toEqual(8n * 10n ** 16n);
+    expect(contractState.fields.attendees.length).toEqual(10);
+    expect(contractState.fields.numAttendees).toEqual(8n);
+    expect(contractState.fields.attendees).toEqual([
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y",
+      ZERO_ADDRESS,
+      ZERO_ADDRESS,
+    ]);
+  });
+
+
+
+  it("test buy a ticket more than 1 ALPH", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+
+    testParams.testArgs.amount = 11n * 10n ** 17n;
+
+    await expectAssertionError(
+      Walph.tests.buyTicket(testParams),
+      testContractAddress,
+      7
+    );
+  });
+
+
+  it("test buy a ticket less than ticket price", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.initialFields.ticketPrice = 10n * 10n ** 18n
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+
+    testParams.testArgs.amount = 9n * 10n ** 18n;
+
+    await expectAssertionError(
+      Walph.tests.buyTicket(testParams),
+      testContractAddress,
+      7
+    );
+  });
+
+
+  it("test buy a ticket without holding token", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+    testParams.inputAssets[0].asset.tokens[0].amount = 0n
+    testParams.testArgs.amount = 1n * 10n ** 18n;
+
+    await expectAssertionError(
+      Walph.tests.buyTicket(testParams),
+      testContractAddress,
+      5
+    );
+  });
+
+
+  it("test buy a ticket when pool full", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.initialFields.balance = 100n * 10n ** 18n;
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+
+    await expectAssertionError(
+      Walph.tests.buyTicket(testParams),
+      testContractAddress,
+      0
+    );
+  });
+
+  it("test distribute prize pool", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true;
+    testParams.initialFields.balance =  0;
+    testParams.initialAsset.alphAmount = 100 * 10 ** 18;
+    //testParams.initialFields.numAttendees = 10
+    testParams.inputAssets[0].address = "1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y"
+    testParams.testArgs.amount = 10 * 10 ** 18
+
+
+    const testResult = await Walph.tests.buyTicket(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.balance).toEqual(0n);
+    expect(contractState.fields.open).toEqual(true);
+    expect(contractState.fields.numAttendees).toEqual(0n);
+    expect(contractState.fields.attendees.length).toEqual(10);
+    expect(contractState.fields.lastWinner).toEqual("1GBvuTs4TosNB9xTCGJL5wABn2xTYCzwa7MnXHphjcj1y");
+  });
+
+
+  it("test withdraw fees", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = true
+    testParams.initialFields.balance =  0
+    testParams.initialAsset.alphAmount = 100 * 10 ** 18
+    testParams.initialFields.feesBalance = 1 * 10 ** 18
+
+    const testResult = await Walph.tests.withdraw(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.feesBalance).toEqual(0n);
+
+  });
+
+
+
+  it("test change token amount to hodl", async () => {
+    const testParams = JSON.parse(JSON.stringify(testParamsFixture));
+    testParams.initialFields.open = false;
+    testParams.initialFields.balance = testParams.initialFields.poolSize;
+    testParams.initialAsset.alphAmount = testParams.initialFields.poolSize + 1;
+    
+    testParams.testArgs.newAmount = 10n
+    const testResult = await Walph.tests.changeMinAmountToHold(testParams);
+    const contractState = testResult.contracts[0] as WalphTypes.State;
+
+    expect(contractState.fields.minTokenAmountToHold).toEqual(10n);
+  });
+});
